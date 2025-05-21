aa0d0bfbaaa88064494657ab9ec6c5bf981c08ce
A. Abdullah Mujtaba <ahmedmujtaba823@gmail.com>
2025-05-21 14:20:08 +0000

    datatypes that exist in js

---
+++ o1_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use static"; // this tells the engine that all the code will run on the newer specifications not required now as all the code runs on the newer specifications
+
+
+let age = 20 //this is how we define an integer
+let name = "me" //this is now a string ""
+let loggedin = false // this is now a boolean 
+
+//console.log(3+3) console.log(name) // this will give an error because no semicolon also poor readability 
+
+// number 
+// string
+// boolean
+// null => standalone value this will return null 
+// undefined 
+// symbol => unique not sure about this 
+// object
+
+console.log(typeof age) //this is how we see the datatype of any variable
+
