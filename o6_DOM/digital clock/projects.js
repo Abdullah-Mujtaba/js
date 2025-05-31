341d1de1edb09233a34f2e0c91dcb8947ddafc4f
A. Abdullah Mujtaba <122427882+Abdullah-Mujtaba@users.noreply.github.com>
2025-05-31 22:41:21 +0500

    Added a digital clock

---
+++ o6_DOM/digital clock/projects.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector('#clock');
+
+//to display this time after every second we can use an event
+
+//the first parameter is a callback function the second parameter is the interval after which this function will run every single time
+setInterval(function () {
+  const date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
